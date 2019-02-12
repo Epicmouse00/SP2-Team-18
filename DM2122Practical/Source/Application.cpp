fe8dc8d5f9505aaffc513dd8f226b0cdd91610c2
Leonidastee <42331603+Leonidastee@users.noreply.github.com>
2019-02-12 21:27:31 +0800

    Raceholder name

--- DM2122Practical/Source/Application.cpp
+++ DM2122Practical/Source/Application.cpp
@@ -69,7 +69,7 @@ void Application::Init()
 
 
 	//Create a window and create its OpenGL context
-	m_window = glfwCreateWindow(800, 600, "Computer Graphics", NULL, NULL);
+	m_window = glfwCreateWindow(800, 600, "Raceholder", NULL, NULL);
 
 	//If the window couldn't be created
 	if (!m_window)
